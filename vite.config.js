0c76cf2f3d3e1e52c70b2ad3ada0c2da94caa9ee
Panadams <83579706+Panadams@users.noreply.github.com>
2025-01-02 19:56:01 +0100

    deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',
-    server: {
-        host: true,
-        open: true
-    }
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: true,
+        publicDir: 'public'
+      },
+    base: '/coin-collector',
 }); 
